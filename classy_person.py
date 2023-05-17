e97e72cc0b16ca4c0b81c8aeeb510a8edf039969
nsjpn6 <122318625+nsjpn6@users.noreply.github.com>
2023-05-16 20:11:06 -0500

    updated

--- classy_person.py
+++ classy_person.py
@@ -8,6 +8,37 @@ don't forget the use of 'self' and to have the methods:
 '''
 
 class Person:
+    def __init__(self, age, name):
+        self.age = age
+        self.name = name
+
+    def increase_age(self):
+        self.age = self.age + 1
+        print(self.age)
+        return ""
+
+    def say_greeting(self):
+        print(f"Hello world! My name is {self.name}!")
+        return ""
+
+    def count_to_age(self):
+        for year in range(1, self.age+1):
+            print(year)
+        return ""
+
+
+nathan = Person(32, "Nathan")
+
+print(nathan.age)
+print(nathan.say_greeting())
+print(nathan.increase_age())
+print(nathan.count_to_age())
+
+
+
+    
+
+
 
 
 
